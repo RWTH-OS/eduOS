b55887a95d71fcb62992d21edd19c871dd00403c
Steffen Vogel <post@steffenvogel.de>
2014-06-30 00:05:56 +0200

    added bitmap allocator

--- mm/memory.c
+++ mm/memory.c
@@ -27,8 +27,21 @@
 
 #include <eduos/stddef.h>
 #include <eduos/stdlib.h>
+#include <eduos/spinlock.h>
+
+#include <asm/atomic.h>
+#include <asm/multiboot.h>
+#include <asm/page.h>
+
 
 static char stack[MAX_TASKS-1][KERNEL_STACK_SIZE];
+static char bitmap[BITMAP_SIZE];
+
+static spinlock_t bitmap_lock = SPINLOCK_INIT;
+
+atomic_int32_t total_pages = ATOMIC_INIT(0);
+atomic_int32_t total_allocated_pages = ATOMIC_INIT(0);
+atomic_int32_t total_available_pages = ATOMIC_INIT(0);
 
 void* create_stack(tid_t id)
 {
@@ -40,4 +53,92 @@ void* create_stack(tid_t id)
 		return NULL;
 
 	return (void*) stack[id-1];
+}
+
+inline static int page_marked(size_t i)
+{
+	size_t index = i >> 3;
+	size_t mod = i & 0x7;
+
+	return  (bitmap[index] & (1 << mod));
+}
+
+inline static void page_set_mark(size_t i)
+{
+	size_t index = i >> 3;
+	size_t mod = i & 0x7;
+
+	bitmap[index] = bitmap[index] | (1 << mod);
+}
+
+inline static void page_clear_mark(size_t i)
+{
+	size_t index = i / 8;
+	size_t mod = i % 8;
+
+	bitmap[index] = bitmap[index] & ~(1 << mod);
+}
+
+size_t get_pages(size_t npages)
+{
+	size_t cnt, off;
+
+	if (BUILTIN_EXPECT(!npages, 0))
+		return 0;
+	if (BUILTIN_EXPECT(npages > atomic_int32_read(&total_available_pages), 0))
+		return 0;
+
+	spinlock_lock(&bitmap_lock);
+
+	off = 1;
+	while (off <= BITMAP_SIZE*8 - npages) {
+		for (cnt=0; cnt<npages; cnt++) {
+			if (page_marked(off+cnt))
+				goto next;
+		}
+
+		for (cnt=0; cnt<npages; cnt++) {
+			page_set_mark(off+cnt);
+		}
+
+		spinlock_unlock(&bitmap_lock);
+
+		atomic_int32_add(&total_allocated_pages, npages);
+		atomic_int32_sub(&total_available_pages, npages);
+
+		return off << PAGE_BITS;
+
+next:		off += cnt+1;
+	}
+
+	spinlock_unlock(&bitmap_lock);
+
+	return 0;
+}
+
+int put_pages(size_t phyaddr, size_t npages)
+{
+	size_t i, ret = 0;
+	size_t base = phyaddr >> PAGE_BITS;
+
+	if (BUILTIN_EXPECT(!phyaddr, 0))
+		return -EINVAL;
+	if (BUILTIN_EXPECT(!npages, 0))
+		return -EINVAL;
+
+	spinlock_lock(&bitmap_lock);
+
+	for (i=0; i<npages; i++) {
+		if (page_marked(base+i)) {
+			page_clear_mark(base+i);
+			ret++;
+		}
+	}
+
+	spinlock_unlock(&bitmap_lock);
+
+	atomic_int32_sub(&total_allocated_pages, ret);
+	atomic_int32_add(&total_available_pages, ret);
+
+	return ret;
 }
