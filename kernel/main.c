2eaa7589ee758aa2c5d388edcf602aaf9a841c7e
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2014-12-05 07:26:24 +0100

    enable cache for our user-level task

--- kernel/main.c
+++ kernel/main.c
@@ -82,13 +82,13 @@ static int wrapper(void* arg)
 	// dirty hack, map userfoo to the user space
 	size_t phys = page_virt_to_phys(((size_t) userfoo) & PAGE_MASK);
 	size_t vuserfoo = 0x40000000; 
-	page_map(vuserfoo, phys, 2, PG_PRESENT | PG_RW | PG_PCD | PG_USER);
+	page_map(vuserfoo, phys, 2, PG_PRESENT | PG_USER);
 	vuserfoo += (size_t)userfoo & 0xFFF;
 
 	// dirty hack, map ustack to the user space
 	phys = page_virt_to_phys((size_t) ustack);
 	size_t vstack = 0x80000000;
-	page_map(vstack, phys, KERNEL_STACK_SIZE >> PAGE_BITS, PG_PRESENT | PG_RW | PG_PCD | PG_USER);
+	page_map(vstack, phys, KERNEL_STACK_SIZE >> PAGE_BITS, PG_PRESENT | PG_RW | PG_USER);
 	vstack = (vstack + KERNEL_STACK_SIZE - 16 - sizeof(size_t));
 
 	return jump_to_user_code(vuserfoo, vstack);
