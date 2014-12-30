9210adc352eab2785fb248887eaf4ed9accfa732
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2014-12-30 21:47:35 +0100

    ring3 is still missing => remove user-level function

--- kernel/main.c
+++ kernel/main.c
@@ -146,9 +146,8 @@ int main(void)
 	kprintf("Current allocated memory: %lu KiB\n", atomic_int32_read(&total_allocated_pages) * PAGE_SIZE / 1024);
 	kprintf("Curren available memory: %lu KiB\n", atomic_int32_read(&total_available_pages) * PAGE_SIZE / 1024);
 
-
 	create_kernel_task(&id1, foo, "foo1", NORMAL_PRIO);
-	create_kernel_task(&id2, wrapper, "userfoo", NORMAL_PRIO);
+	//create_kernel_task(&id2, wrapper, "userfoo", NORMAL_PRIO);
 
 	while(1) { 
 		HALT;
