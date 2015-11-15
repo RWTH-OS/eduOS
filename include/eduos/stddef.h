0f6fad13f527163e9e139244dfa9f9c754328aca
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2015-11-15 23:38:21 +0100

    add autotool files

--- include/eduos/stddef.h
+++ include/eduos/stddef.h
@@ -41,6 +41,22 @@
 extern "C" {
 #endif
 
+#define VIDEO_MEM_ADDR		0xB8000 // the video memora address
+#define CACHE_LINE		64
+#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#define BYTE_ORDER BIG_ENDIAN
+#else
+#define BYTE_ORDER LITTLE_ENDIAN
+#endif
+
+#ifdef __GNUC__
+#define BUILTIN_EXPECT(exp, b)  __builtin_expect((exp), (b))
+#else
+#define BUILTIN_EXPECT(exp, b)	(exp)
+#endif
+#define NORETURN		__attribute__((noreturn))
+#define STDCALL			__attribute__((stdcall))
+
 #define NULL 		((void*) 0)
 
 /// represents a task identifier
